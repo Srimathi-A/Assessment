61e090233ba9d65ab549c94ee0c519dbcb92ecb7
Srimathi-A <118583541+Srimathi-A@users.noreply.github.com>
2024-03-04 14:58:16 +0530

    Add files via upload

---
+++ assessment2.sql
@@ -0,0 +1,36 @@
+--create table students1 (  stud_id int, stud_name varchar(25),
+                         -- qualification varchar(25), email varchar(30),
+                          -- contact int,  course_id int,  dateofjoining date
+--);
+--insert into students1 values('1','Fathima','MCA','fathima@gmail.com','987654310','1','2024-02-12'),
+                          --  ('2','Raveena','BSc','raveena@gmail.com','877543291','2','2024-02-20'),
+                            --('3','Vinoth','BCom','vinoth@gmail.com','877643213','1','2024-01-17'),
+                            --('4','Tarun','BCom','tarun@gmail.com','677896543','3','2023-09-24'),
+                            --('5','Rajesh','BCom','rajesh@gmail.com','877903456','3','2023-08-17'),
+                            --('6','Kalyani','BSc','kalyani@gmail.com','788901235','2','2023-10-23'),
+                            --('7','Hemanth','MCA','hemanth@gmail.com','877654310','1','2023-11-27');	
+--select * from students1
+--create table course (  course_id int, course_name varchar(25),
+                       --fees int, Duration varchar(10)
+                       -- );
+--insert into course values('1','FSD','80000','6 months'),
+                         --('2','PGA','125000','6 months'),
+						 --('3','Cibop','90000','6 months'),
+						 --('4','Digital maketing','85000','6 months');
+--select * from course	
+--select Max(fees)
+--from course;
+--select sum(fees)
+--from course
+--select  fees,Duration from course
+--select fees,Duration from course
+--select course_id course_name from students1
+--select * from students1 order by stud_id asc limit 4 offset 3;
+--update students1
+--set dateofjoining = '2024/02/23'
+--WHERE stud_id = 6;
+--select* from students1
+
+
+                                 
+
